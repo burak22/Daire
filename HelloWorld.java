0ad0f7166ed9b3cd07399fcf86d2d1e2ac30929e
burak22 <43789237+burak22@users.noreply.github.com>
2022-08-26 15:54:50 +0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,14 +3,13 @@ import java.util.Scanner;
 public class HelloWorld{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        float Pi = 3.14f;
 
         System.out.print("Yarıçap Giriniz: ");
         float r = input.nextFloat();
         System.out.print("Merkez Açı Giriniz: ");
         float a = input.nextFloat();
 
-        float alan = (Pi*r*r*a)/360;
+        float alan = (3.14*r*r*a)/360;
         System.out.println("Daire Diliminin - Dairenin Alanı:" + alan);
     }
 
